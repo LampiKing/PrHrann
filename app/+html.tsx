15d2266d9f27f3b8e56c118695a215f52a92878f
LampiKing <lampilinko@gmail.com>
2025-12-29 14:59:52 +0100

    update 0.9

---
+++ app/+html.tsx
@@ -0,0 +1,19 @@
+﻿import React from "react";
+import { Html, Head, Main, NextScript } from "expo-router/html";
+
+export default function Root() {
+  return (
+    <Html>
+      <Head>
+        <meta name="theme-color" content="#0a0a12" />
+        <style>{`
+          html, body, #root {
+            background-color: #0a0a12;
+          }
+        `}</style>
+      </Head>
+      <Main />
+      <NextScript />
+    </Html>
+  );
+}
